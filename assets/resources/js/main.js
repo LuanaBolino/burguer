fcf2e1fdd17052f628bf98bcd9556c15583e9bf7
Luana <luanabolino.dev@gmail.com>
2025-03-14 22:37:27 -0300

    Adicionado link nas tags 'a'

--- assets/resources/js/main.js
+++ assets/resources/js/main.js
@@ -1,20 +1,17 @@
 document.addEventListener("scroll", () => {
-    // Verifica se a largura da tela é maior que 1200px
     if (window.innerWidth > 1200) {
       const navbar = document.getElementById("nav-red");
-      const links = document.getElementsByClassName("a"); // Retorna uma coleção de elementos
+      const links = document.getElementsByClassName("a"); 
       
-      if (window.scrollY > 50) { // Ativa a classe após 50px de scroll
+      if (window.scrollY > 50) { 
         navbar.classList.add("scrolled");
   
-        // Adiciona a classe "a-scrolled" a cada link
         for (let link of links) {
           link.classList.add("a-scrolled");
         }
       } else {
         navbar.classList.remove("scrolled");
   
-        // Remove a classe "a-scrolled" de cada link
         for (let link of links) {
           link.classList.remove("a-scrolled");
         }
@@ -29,24 +26,22 @@ document.addEventListener("scroll", () => {
 
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        menuIcon.src = "assets/resources/images/svg/menu.svg"; // Ícone de menu
+        menuIcon.src = "assets/resources/images/svg/menu.svg"; 
     } else {
         menuMobile.classList.add('open');
-        menuIcon.src = "assets/resources/images/svg/close.svg"; // Ícone de close
+        menuIcon.src = "assets/resources/images/svg/close.svg";
     }
 }
 
-// Fecha o menu ao clicar fora dele
 document.addEventListener('click', (event) => {
     const menuMobile = document.querySelector('.mobile-menu');
     const menuIcon = document.querySelector('.menu-icon');
 
-    // Verifica se o clique foi fora do menu e do ícone
     if (
-        !menuMobile.contains(event.target) && // Clique fora da `mobile-menu`
-        !menuIcon.contains(event.target)     // Clique fora do ícone do menu
+        !menuMobile.contains(event.target) && 
+        !menuIcon.contains(event.target)     
     ) {
         menuMobile.classList.remove('open');
-        menuIcon.src = "assets/resources/images/svg/menu.svg"; // Volta para o ícone de menu
+        menuIcon.src = "assets/resources/images/svg/menu.svg"; 
     }
 });
